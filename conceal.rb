16eb785c164e2d32500eeabacd9c6ce95c4f1c0b
Joe Garcia <infamousjoeg@users.noreply.github.com>
2020-07-09 21:04:07 -0400

    Update conceal.rb

--- conceal.rb
+++ conceal.rb
@@ -1,8 +1,8 @@
 class Conceal < Formula
   desc "Simply store secrets in MacOS Keychain Access for use by Summon"
   homepage "https://github.com/infamousjoeg/conceal"
-  url "https://github.com/infamousjoeg/conceal/releases/download/v2.0.0-dev/conceal"
-  sha256 "2a7c162a41fdfd0ce25f4fc568baef4b6badc2e440b774b074c97200511db3b0"
+  url "https://github.com/infamousjoeg/conceal/releases/download/v2.0.1-dev/conceal"
+  sha256 "1ba365a6a42b113a52cef4d0a45fbf488cab5a0574e00c0a667c25157dc96d3c"
   bottle :unneeded
 
   def install
