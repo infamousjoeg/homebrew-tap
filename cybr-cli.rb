652c6895a72144c4632544c1beb38a3e37592961
Joe Garcia <infamousjoeg@users.noreply.github.com>
2021-01-13 10:52:27 -0500

    Update cybr-cli.rb

--- cybr-cli.rb
+++ cybr-cli.rb
@@ -1,8 +1,8 @@
 class CybrCli < Formula
   desc "CyberArk Privileged Access Security (PAS) Command-Line Interface (CLI)"
   homepage "https://github.com/infamousjoeg/cybr-cli"
-  url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.0.4-alpha/cybr"
-  sha256 "14f7145edb2d665886428110d221d4499ef64aacc0391d4f7b117265941f1512"
+  url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.0.5-alpha/cybr"
+  sha256 "ede1c0df476ce984917de6c911770e70cc652fe2cea11403fc473e0e6d4dd5a9"
   license "Apache-2.0"
   bottle :unneeded
 
