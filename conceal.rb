6b95ae00d6d359b5d2b37f49a6dd04b0f7d59cad
Joe Garcia <joe@joe-garcia.com>
2022-02-21 20:40:52 -0500

    rm bottle:unneeded

--- conceal.rb
+++ conceal.rb
@@ -1,12 +1,17 @@
 class Conceal < Formula
   desc "Simply store secrets in MacOS Keychain Access"
   homepage "https://github.com/infamousjoeg/conceal"
-  url "https://github.com/infamousjoeg/conceal/releases/download/v3.0.0-release/conceal"
-  sha256 "0faf46fda8c335f2edfd8ecc9f3f6bb0fefe453889f4d66e42256e9dfcb5ebc3"
-  bottle :unneeded
+  version "3.0.0"
 
-  def install
-    bin.install "conceal"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/infamousjoeg/conceal/releases/download/v3.0.0-release/conceal"
+      sha256 "0faf46fda8c335f2edfd8ecc9f3f6bb0fefe453889f4d66e42256e9dfcb5ebc3"
+
+      def install
+        bin.install "conceal"
+      end
+    end
   end
 
   test do
--- cybr-cli.rb
+++ cybr-cli.rb
@@ -1,13 +1,36 @@
 class CybrCli < Formula
   desc "CyberArk Privileged Access Security (PAS) Command-Line Interface (CLI)"
   homepage "https://github.com/infamousjoeg/cybr-cli"
-  url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.8-beta/cybr"
-  sha256 "8e015a42cda8059d7b9d7518390d249e12147db7a3a373921867fea4968d7561"
-  license "Apache-2.0"
-  bottle :unneeded
+  version "0.1.8-beta"
 
-  def install
-    bin.install "cybr"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.8-beta/darwin_arm64_cybr"
+      sha256 "8e015a42cda8059d7b9d7518390d249e12147db7a3a373921867fea4968d7561"
+
+      def install
+        bin.install "cybr"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.8-beta/cybr"
+      sha256 "8e015a42cda8059d7b9d7518390d249e12147db7a3a373921867fea4968d7561"
+
+      def install
+        bin.install "cybr"
+      end
+    end
+  end
+
+  on_linux do
+    if Hardware:CPU.intel?
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.8-beta/linux_cybr"
+      sha256 "8e015a42cda8059d7b9d7518390d249e12147db7a3a373921867fea4968d7561"
+
+      def install
+        bin.install "cybr"
+      end
+    end
   end
 
   test do
