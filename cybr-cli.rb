834d1e5d79866c699061427f54c0a628f3d222a7
Joe Garcia <infamousjoeg@users.noreply.github.com>
2022-04-14 16:09:45 -0400

    Update to v0.1.9-beta

--- cybr-cli.rb
+++ cybr-cli.rb
@@ -1,20 +1,20 @@
 class CybrCli < Formula
   desc "CyberArk Privileged Access Security (PAS) Command-Line Interface (CLI)"
   homepage "https://github.com/infamousjoeg/cybr-cli"
-  version "0.1.8-beta"
+  version "0.1.9-beta"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.8-beta/darwin_arm64_cybr"
-      sha256 "8e015a42cda8059d7b9d7518390d249e12147db7a3a373921867fea4968d7561"
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.9-beta/darwin_arm64_cybr"
+      sha256 "9ba8255aa26d9f3be30e35507e9f3b81f77a48f869672011a7690ff48c1a2353"
 
       def install
         bin.install "cybr"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.8-beta/cybr"
-      sha256 "8e015a42cda8059d7b9d7518390d249e12147db7a3a373921867fea4968d7561"
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.9-beta/cybr"
+      sha256 "9ba8255aa26d9f3be30e35507e9f3b81f77a48f869672011a7690ff48c1a2353"
 
       def install
         bin.install "cybr"
@@ -24,8 +24,8 @@ class CybrCli < Formula
 
   on_linux do
     if Hardware:CPU.intel?
-      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.8-beta/linux_cybr"
-      sha256 "8e015a42cda8059d7b9d7518390d249e12147db7a3a373921867fea4968d7561"
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.9-beta/linux_cybr"
+      sha256 "9ba8255aa26d9f3be30e35507e9f3b81f77a48f869672011a7690ff48c1a2353"
 
       def install
         bin.install "cybr"
