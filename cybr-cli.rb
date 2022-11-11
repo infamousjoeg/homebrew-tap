69754198ba4e3025343fbfcc06d8d3af77bc29b1
Joe Garcia <infamousjoeg@users.noreply.github.com>
2022-11-11 11:43:30 -0500

    Update cybr-cli.rb

--- cybr-cli.rb
+++ cybr-cli.rb
@@ -1,20 +1,20 @@
 class CybrCli < Formula
   desc "CyberArk Privileged Access Security (PAS) Command-Line Interface (CLI)"
   homepage "https://github.com/infamousjoeg/cybr-cli"
-  version "0.1.12-beta"
+  version "0.1.13-beta"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.12-beta/cybr-cli_darwin_arm64.tar.gz"
-      sha256 "10c578d544267c837f869c88978dfc845c082f664db74d66233c41894730c86a"
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.13-beta/cybr-cli_darwin_arm64.tar.gz"
+      sha256 "a8a5e792cf21b0577623531b1dc3eaa6baaa1e8e2ed28afe47c4af26116a0eb4"
 
       def install
         bin.install "cybr"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.12-beta/cybr-cli_darwin_amd64.tar.gz"
-      sha256 "f91189aa4db226318a7e24a0ded3a55a3a2494baa7d82a2ace7dc1c23d01b9a9"
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.13-beta/cybr-cli_darwin_amd64.tar.gz"
+      sha256 "25077c516c7e6e8bdb36d97f429a42451dbfbdcfdd6a12c3c7a7cdefb5afdc76"
 
       def install
         bin.install "cybr"
@@ -24,16 +24,16 @@ class CybrCli < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.12-beta/cybr-cli_linux_arm64.tar.gz"
-      sha256 "e1803918c01c8913881a2fadc3b956c141c7d2ea79f57feff81bcde4319ca5f9"
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.13-beta/cybr-cli_linux_arm64.tar.gz"
+      sha256 "fe9e4f3b840e81212820a93e2daacc8e906c5ed9525236358912806427caaf65"
 
       def install
         bin.install "cybr"
       end
     end
     if Hardware:CPU.intel?
-      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.12-beta/cybr-cli_linux_amd64.tar.gz"
-      sha256 "f7e468683e47afbecfdc0f6a9a5d9be3b7c636fb5d24a85beeb7817f685c2e59"
+      url "https://github.com/infamousjoeg/cybr-cli/releases/download/v0.1.13-beta/cybr-cli_linux_amd64.tar.gz"
+      sha256 "71910d9bb0ea90e0109c3dabdafcb6eaf63d8f02270f0da4a0d7b6f335f537b7"
 
       def install
         bin.install "cybr"
