bd3b8c1506606fa8fd84d63daf4a25f6da8c2bdf
Joe Garcia <infamousjoeg@users.noreply.github.com>
2024-01-24 12:00:16 -0500

    Update cybr-cli.rb

--- cybr-cli.rb
+++ cybr-cli.rb
@@ -1,12 +1,12 @@
 class CybrCli < Formula
   desc "CyberArk Privileged Access Security (PAS) Command-Line Interface (CLI)"
   homepage "https://github.com/infamousjoeg/cybr-cli"
-  version "1.0.1-release"
+  version "1.0.2-release"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/infamousjoeg/cybr-cli/releases/download/v#{version}/cybr-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "8f99f29e6ceb25872beecdf20de549bab8960669e38cb0feb67e196e7179301e"
+      sha256 "edbad81f2f28882b40b7b9f7803bebc8a9934d048d0344e97a3534c326bbebae"
 
       def install
         bin.install "cybr"
@@ -14,7 +14,7 @@ class CybrCli < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/infamousjoeg/cybr-cli/releases/download/v#{version}/cybr-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "f3ec8292319357166d194a24d68d0c3bd5e039460a643378d8a75a11ff288cbf"
+      sha256 "37484f5aece7a55358ac4920b587519b8395cb723698afcb65f4e4af7832a77a"
 
       def install
         bin.install "cybr"
@@ -25,7 +25,7 @@ class CybrCli < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/infamousjoeg/cybr-cli/releases/download/v#{version}/cybr-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "379868b2bb0adcfe1cb637079e95ff5179f378eff71737dbd5c345e87acbe201"
+      sha256 "ae1f7363d1b4a85449e65e1025f21815a21fe261c2e899e830ad55059df80004"
 
       def install
         bin.install "cybr"
@@ -33,7 +33,7 @@ class CybrCli < Formula
     end
     if Hardware:CPU.intel?
       url "https://github.com/infamousjoeg/cybr-cli/releases/download/v#{version}/cybr-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "55ee474ef82c9186082d5c3699f47dfb74474c13089d75cec2d49b1600bc9391"
+      sha256 "6a554997ec05124f77c17ddf8f610d440a2ea04570be948a1d5193ffc6664451"
 
       def install
         bin.install "cybr"
