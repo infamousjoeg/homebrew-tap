68b8c03c9279be4ca1ffcbc392cef407d0058919
Joe Garcia <infamousjoeg@users.noreply.github.com>
2024-02-06 11:15:43 -0500

    Update conceal.rb

--- conceal.rb
+++ conceal.rb
@@ -1,11 +1,11 @@
 class Conceal < Formula
   desc "Simply store secrets in MacOS Keychain Access"
   homepage "https://github.com/infamousjoeg/conceal"
-  version "3.0.0"
+  version "3.0.0-release"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/infamousjoeg/conceal/releases/download/v3.0.0-release/conceal"
+      url "https://github.com/infamousjoeg/conceal/releases/download/v#{version}/conceal"
       sha256 "0faf46fda8c335f2edfd8ecc9f3f6bb0fefe453889f4d66e42256e9dfcb5ebc3"
 
       def install
