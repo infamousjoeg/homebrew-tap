eb11888cc0b5b5113dcea929ef4c0970ff0057f8
Joe Garcia <infamousjoeg@users.noreply.github.com>
2024-05-10 10:59:29 -0400

    Create stow.rb

---
+++ stow.rb
@@ -0,0 +1,30 @@
+class Stow < Formula
+  desc "Command-line secrets manager for Summon (https://cyberark.github.io/summon) powered by the Keychain tools already available on macOS systems"
+  homepage "https://github.com/infamousjoeg/stow"
+  url "https://github.com/loteoo/ks/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "95885c91c72f8a117c3aade8b189654e281ccf2fafab3d7646f5e13ee8f524a7"
+  license "MIT"
+
+  depends_on :macos
+  uses_from_macos "bash"
+  uses_from_macos "openssl"
+
+  def install
+    bin.install "stow"
+  end
+
+  def post_install
+    ohai "stow has been installed 🎉"
+    ohai "Please run \"stow init\" to create your first keychain."
+  end
+
+  def caveats
+    <<~EOS
+      You can setup basic completions by adding "source <(stow completion)" to your shell profile.
+    EOS
+  end
+
+  test do
+    assert_match "Keychain Secrets manager", shell_output("#{bin}/ks help")
+  end
+end
