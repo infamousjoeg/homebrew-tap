952341785c506fd9a776ca0fdb05264bd48352d2
Joe Garcia <infamousjoeg@users.noreply.github.com>
2025-08-28 09:11:16 -0400

    conceal 4.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- conceal.rb
+++ conceal.rb
@@ -1,11 +1,11 @@
 class Conceal < Formula
   desc "Simply manage secrets in MacOS Keychain Access"
   homepage "https://github.com/infamousjoeg/conceal"
-  version "4.1.0"
+  version "4.1.1"
 
   on_macos do
-    url "https://github.com/infamousjoeg/conceal/archive/refs/tags/v4.1.0.tar.gz"
-    sha256 "b5933e811d26adde3f16b1fcece8d396bba010f558485e16466666f4d935b79a"
+    url "https://github.com/infamousjoeg/conceal/archive/refs/tags/v4.1.1.tar.gz"
+    sha256 "570e73679c085e7319989c7a3e1d6bb878df7e562b285eff02072ad09b4fb199"
 
     def install
       bin.install "conceal"
