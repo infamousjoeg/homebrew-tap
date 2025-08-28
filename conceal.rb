0031ce64bb4ceff1b4f091afc0f92e8a53c457c2
Joe Garcia <infamousjoeg@users.noreply.github.com>
2025-08-28 13:19:29 -0400

    Fix conceal formula to point to v4.1.0 instead of deleted v4.2.3 tag

--- conceal.rb
+++ conceal.rb
@@ -1,11 +1,11 @@
 class Conceal < Formula
   desc "Simply manage secrets in MacOS Keychain Access"
   homepage "https://github.com/infamousjoeg/conceal"
-  version "4.2.3"
+  version "4.1.0"
 
   on_macos do
-    url "https://github.com/infamousjoeg/conceal/archive/refs/tags/v4.2.3.tar.gz"
-    sha256 "fdf345c5d17902b550cc99053306994be8bb7325d8dfa6d5b5adab7452c480e7"
+    url "https://github.com/infamousjoeg/conceal/archive/refs/tags/v4.1.0.tar.gz"
+    sha256 "a3de090e34b758bfe2c7f3d0a5af6580838323fcf430facf1a55fe30f85aea9a"
 
     def install
       bin.install "conceal"
