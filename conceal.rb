42a69fa177f7e3de4631a49400a3f27315d9f733
Joe Garcia <infamousjoeg@users.noreply.github.com>
2025-08-28 13:46:18 -0400

    Fix conceal formula to use binary releases instead of source tarball

--- conceal.rb
+++ conceal.rb
@@ -4,8 +4,13 @@ class Conceal < Formula
   version "4.1.0"
 
   on_macos do
-    url "https://github.com/infamousjoeg/conceal/archive/refs/tags/v4.1.0.tar.gz"
-    sha256 "a3de090e34b758bfe2c7f3d0a5af6580838323fcf430facf1a55fe30f85aea9a"
+    if Hardware::CPU.arm?
+      url "https://github.com/infamousjoeg/conceal/releases/download/v4.1.0/conceal_Darwin_arm64.tar.gz"
+      sha256 "340354d150578ff8484795314e17a8f38ed83f0db71f1a370deadd5786c0b429"
+    else
+      url "https://github.com/infamousjoeg/conceal/releases/download/v4.1.0/conceal_Darwin_x86_64.tar.gz"
+      sha256 "53b54d220cf622c2d829e52c932d6fbbc94ae440b6c00528d04d989efb75ba97"
+    end
 
     def install
       bin.install "conceal"
